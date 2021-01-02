4701edd6f0b95ff66f06670358b4156fab51d957
giogokul13 <63816836+giogokul13@users.noreply.github.com>
2021-01-02 18:06:50 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHomeComponent } from './main-home/main-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHomeComponent
   ],
   imports: [
     BrowserModule,
