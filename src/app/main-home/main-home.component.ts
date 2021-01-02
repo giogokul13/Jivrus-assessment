5466e0e27e00224099ad993dfde96662716f68ac
giogokul13 <63816836+giogokul13@users.noreply.github.com>
2021-01-03 00:27:13 +0530

    added the UI part and Ui functionality

--- src/app/main-home/main-home.component.ts
+++ src/app/main-home/main-home.component.ts
@@ -6,8 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-home.component.css']
 })
 export class MainHomeComponent implements OnInit {
-
+ iscoursedisplay =  false ; isstudentdisplayed =  false;
   constructor() { }
+  
+  addcourse(){
+    console.log('showcourse');
+    this.iscoursedisplay = true
+  }
+  addstudent(){
+    this.isstudentdisplayed = true
+  }
+
+  chide(){
+    this.iscoursedisplay = false;
+  }
+  shide(){
+    this.isstudentdisplayed = false;
+  }
+
 
   ngOnInit(): void {
   }
